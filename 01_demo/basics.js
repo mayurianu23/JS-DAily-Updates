150e51924493c6b05ee4f9733cd33e538dbe3834
mayurianu23 <tarlemayuri74@gmail.com>
2025-12-03 23:03:23 +0530

    basic

---
+++ 01_demo/basics.js
@@ -0,0 +1,23 @@
+var x = 2;
+console.log(x);
+
+let a = "hello world";
+console.log(a);
+
+let bool = true;
+console.log(bool);
+
+let arr = ["apple","banana","cherry"];
+console.log(arr);
+
+var d = null;
+console.log(d);
+
+let obj = {
+    firstname:"mayuri",
+    lastname:"tarle",
+};
+console.log(obj);
+
+const pi = 3.14;
+console.log(pi);
