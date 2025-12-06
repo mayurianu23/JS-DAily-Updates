e0abe4da3c3939ae80251af59adb7fab3ca8a07d
mayurianu23 <tarlemayuri74@gmail.com>
2025-12-06 23:03:07 +0530

    01

---
+++ 02_array/01.js
@@ -0,0 +1,25 @@
+
+let stud = ["jon","jacke","mayuri","anu","om"];
+// console.log(stud[0]);//To acces any element of array using index number
+//modify array
+// stud[1]="ortan"   
+stud.push("siya")//insert at last of array 
+
+stud.pop()//delete last elemt olf array
+
+stud.unshift("ram")// to insert element in array at first position
+
+stud.shift() //removel first element from array at first position
+// console.log(stud.indexOf("jon")); //It wiil show the index no. of elemt
+
+// console.log(stud.includes("on"));//Determines whether an array includes a certain element, returning true or false as appropriate
+
+// console.log( stud.slice(1,3));//Returns a copy of a section of an array. For both start and end end will be excluded 
+
+console.log( stud.splice(1,3));
+
+
+
+
+
+
