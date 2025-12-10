c0fdb37038cf14a9e3f5a47fe33c203a0b1cd731
mayurianu23 <tarlemayuri74@gmail.com>
2025-12-10 22:58:00 +0530

    Reduce_method

--- 02_array/Reduce_method.js
+++ 02_array/Reduce_method.js
@@ -1,14 +1,34 @@
 //Creates a new array containing only the elements that satisfy a given condition.
 
 //sum of array
-const arr = [1,2,3];
+const arr1 = [5,5,6,7];
 function issum(){
     let sum = 0;
-    for (let i = 0; i < arr.length; i++) {
-        sum = sum  + arr[i]
+    for (let i = 0; i < arr1.length; i++) {
+        sum = sum  + arr1[i]
     }
     return sum;
 }
-console.log(issum(arr));
+console.log(issum(arr1));
 
-let output = arr.reduce(function(acc ,curr))
+//Insted of using this we simpaly use  this Reduce method()
+const arr = [5,5,6,7];
+let res = arr.reduce((acc,curr)=> 
+     acc+curr,0)
+console.log(res);
+
+//another way using simapl function
+const arrr = [5,5,6,7];
+let op = arrr.reduce(function(acc,curr){
+},0);
+console.log(op);
+
+
+//2.max of array
+let output = arr.reduce(function(max,curr){
+    if(curr > max){
+        max = curr;
+    }
+    return max;
+}, 0);
+console.log(output);
