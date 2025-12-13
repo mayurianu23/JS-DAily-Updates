1b4127bd5882f21839bd71d71f659ce65d87e481
mayurianu23 <tarlemayuri74@gmail.com>
2025-12-13 22:24:44 +0530

    set

--- Objects/set.js
+++ Objects/set.js
@@ -6,8 +6,8 @@ A Set is a collection of values where:
    You can store any type — number, string, object, etc*/
 
 
-//    let number = new Set([1,2,3,4,5,1,1,1])
-//    console.log(number);//Set(5) {1,2,3,4,5}
+   let number = new Set([1,2,3,4,5,1,1,1])
+   console.log(number);//Set(5) {1,2,3,4,5}
 
 //To add values in emptySet
 let myset = new Set();
@@ -18,7 +18,7 @@ myset.add("cherry")
 console.log(myset.has("apple"));
 //size()
 console.log(myset.size);
-//delete(0)
+//delete()
 console.log(myset.delete("apple"));
 console.log(myset);
 //clear()
@@ -26,3 +26,36 @@ myset.clear()
 console.log(myset);
 
 
+let color = new Set(["red","green",
+"blue"]);
+// for(let color of set){
+// console.log(set);  //  OP: Set(3) { 'red', 'green', 'blue' }
+//                   //Set(3) { 'red', 'green', 'blue' }
+//                   //Set(3) { 'red', 'green', 'blue' }
+//*********** To print all element
+// console.log(color);//OP:red green blue
+// }
+color.forEach(color =>
+    console.log(color))
+
+    //*****************************MAP()******************88 */
+
+
+//1 st way to create Map()
+let userMap = new Map();
+userMap.set("name" ,"mayuri")
+userMap.set("age" ,34)
+userMap.set("city" ,"NewYork")
+console.log(userMap);//Map(3) { 'name' => 'mayuri', 'age' => 34, 'city' => 'NewYork' }
+
+
+//2 st way to create Map()
+let userMap = new Map([
+   ["fname" ,"mayuri"],
+   ["age" ,34],
+   ["city" ,"NewYork"]
+]);
+console.log(userMap);
+for(let val of userMap){
+   console.log(val);
+}
---
+++ multidimentional_array/multi.js
@@ -0,0 +1,8 @@
+let arr = [
+    ["jon",90],
+    ["sai",30],
+    ["om",32],
+
+]
+console.log(arr[1]);//[ 'sai', 30 ]
+console.log(arr[1][0]);//Sai
