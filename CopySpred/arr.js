09c0f74047704dfbc7b062e905d7de1bbc33a4ef
mayurianu23 <tarlemayuri74@gmail.com>
2025-12-16 23:00:09 +0530

    copySpred

---
+++ CopySpred/arr.js
@@ -0,0 +1,19 @@
+//Copy
+
+let arr1 = ["mayuri" , 23, true];
+let arr2 = arr1.concat(["item"]);
+arr2.push("burg");
+console.log(arr1);
+console.log(arr2);
+
+//Spred operator
+
+let array = ["jon",23,true];
+let arr = [...array]
+arr.push("anu");
+console.log(array);
+console.log(arr);
+
+
+
+
