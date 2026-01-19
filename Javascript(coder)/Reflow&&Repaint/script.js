25a45fac5ed56060434bf4bf45607cf6290e084d
mayurianu23 <tarlemayuri74@gmail.com>
2026-01-19 19:44:18 +0530

    Reflow&&Repaint

--- Javascript(coder)/Reflow&&Repaint/script.js
+++ Javascript(coder)/Reflow&&Repaint/script.js
@@ -1,9 +1,28 @@
 //code 1
-const t1 = performance.now();
+//performance.now()
+ const t3 = performance.now()
 for(let i=1; i<=100;i++){
     let para = document.createElement('p');
     para.textContent = "This is Para" + i;
     document.body.appendChild(para);
 }
+const t4 = performance.now();
+console.log("total time talken by code 1"+(t4-t3));
+
+
+
+//code 2
+const t1 = performance.now()
+let  mydiv = document.createElement('div');
+for(let i=1; i<=100; i++){
+        let para = document.createElement('p');
+    para.textContent = "This is Para" + i;
+    mydiv.appendChild(para);
+}
+document.body.appendChild(mydiv);
+
 const t2 = performance.now();
-console.log("total time talken by code 1"+(t2-t1));
+
+ console.log("total time talken by code 2"+(t2-t1));
+
+
